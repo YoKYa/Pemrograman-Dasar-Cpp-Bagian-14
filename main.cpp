75e6b586e5ff234b3e3c91ed68b6e51a0f6842ca
YoKYa <yogiekaprastiya1@gmail.com>
2020-03-28 12:44:57 +0700

    Pemrograman Dasar Cpp Bagian 14
    Pemrograman Dasar Cpp Bagian 14 ( Assignment Operator )

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    system("cls");
+
+    int A = 2;  // Deklarasi Variabel
+    
+    // Assignment Operator Bisa Tambah, Kurang, Bagi, Kali
+    A += 6; // Assignment Sama Dengan A = A + 6;
+    cout << A;
+
+    cin.get();
+    return 0;
+}
+    
